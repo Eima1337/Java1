9ff6251f702cc42cf8d101fff375807ba247c05f
Eima1 <e.bakevicius@gmail.com>
2020-09-09 12:43:40 +0300

    Array's namu darbas

---
+++ src/lt/eimantas/app1/ND4.java
@@ -0,0 +1,40 @@
+package lt.eimantas.app1;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.Arrays;
+
+public class ND4 {
+    public static void main(String[] args) {
+
+        int[] array1 = new int[]{1, 2, 5, 19, 10};
+
+        for (int i = 0; i < array1.length; i++) {
+            System.out.println(array1[i]);
+        }
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Iveskite masyvo ilgi:");
+        int num = sc.nextInt();
+        int[] array2 = new int[num];
+
+
+
+        for (int i = 0; i < array2.length; i++) {
+            System.out.println("Iveskite skaicius:");
+            array2[i] = sc.nextInt();
+        }
+        System.out.println("Jusu masyvas:");
+        Arrays.sort(array2);
+        for (int i = 0; i < array2.length; i++) {
+            System.out.println(array2[i]);
+        }
+        int sum = IntStream.of(array2).sum();
+        double avg = IntStream.of(array2).average().orElse(Double.NaN);
+        System.out.println("Masyvo suma: " + sum);
+        System.out.println("Masyvo vidurkis: " + avg);
+
+    }
+}
+
